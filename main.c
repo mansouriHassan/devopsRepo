e3b06770d2ef85ca2bc21f0b1166c9f2a1d9f913
mansouri hassan <mansouri.hassan.ing@gmail.com>
2020-12-02 21:00:50 +0100

    modif main

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main()
-{
-	printf("devops jenkins lab\n");
+int main() {
+    int i;
+    for(i=0; i<20; i++) {
+        printf("Hello DevOps !! \n");
+        sleep(1);
+    }
+
 	return 0;
 }
